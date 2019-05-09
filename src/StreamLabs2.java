17c8a9ea25c50a0655ed38238f74999aac6bf71b
Strelok310 <roman20330@mail.ru>
2019-05-10 00:30:39 +0300

    update
    Stream Labs 2

--- src/StreamLabs2.java
+++ src/StreamLabs2.java
@@ -15,8 +15,11 @@ public class StreamLabs2 {
     public static void main(String args[]) throws Exception {
         System.out.println("Stream from ArrayList");
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,3,4,5,6,7,2,1,4));
+        System.out.println(list);
         Stream oddStream = list.stream().filter((x) -> x % 2 == 1);
         System.out.println(Arrays.toString(oddStream.toArray(Integer[]::new)));
+
+        //==============================================================================================================
 
         System.out.println("\nSort Persons List by createdAt");
         ArrayList<Person> personsList = new ArrayList<>(Arrays.asList(
@@ -25,35 +28,53 @@ public class StreamLabs2 {
             new Person("Peter","Peter", LocalDateTime.of(2019,3,29,12,3,8)),
             new Person("Nikita","Nikita", LocalDateTime.of(2019,2,28,9,15,46))
         ));
+        System.out.println(personsList);
         LocalDateTime date = LocalDateTime.of(2019,3,9,15,26,46);
         Stream dateStream = personsList.stream().filter((x) -> x.createdAt.isBefore(date));
         System.out.println(Arrays.toString(dateStream.toArray(Person[]::new)));
 
+        //==============================================================================================================
+
         System.out.println("\nSort Persons List by createdAt and logged");
         personsList.get(0).logged = true;
+        System.out.println(personsList);
         Stream dateLoggedStream = personsList.stream().filter((x) -> x.createdAt.isBefore(date) && x.logged);
         System.out.println(Arrays.toString(dateLoggedStream.toArray(Person[]::new)));
 
+        //==============================================================================================================
+
         System.out.println("\nStream of even elements id");
         ArrayList<Integer> evenList = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
+        System.out.println(evenList);
         Stream<Integer> evenStream = evenList.stream().skip(evenList.size()/2);
         System.out.println(Arrays.toString(evenStream.toArray(Integer[]::new)));
 
+        //==============================================================================================================
+
         System.out.println("\nReturn selected element of Array");
+        System.out.println(evenList);
         int value = evenList.stream().skip(5).findFirst().orElse(42);
         System.out.println("6) " + value);
         value = evenList.stream().skip(12).findFirst().orElse(42);
         System.out.println("12) " + value);
 
+        //==============================================================================================================
+
         System.out.println("\nReturn array with unique elements");
         ArrayList<Integer> duplicatesList = new ArrayList<>(Arrays.asList(1,1,3,4,3,6,7,8));
+        System.out.println(duplicatesList);
         Stream<Integer> duplicatesStream = duplicatesList.stream().distinct();
         System.out.println(Arrays.toString(duplicatesStream.toArray(Integer[]::new)));
 
+        //==============================================================================================================
+
         System.out.println("\nAdd _outdated to each element of array");
         ArrayList<String> addList = new ArrayList<>(Arrays.asList("log1", "log2", "log3"));
+        System.out.println(addList);
         Stream<String> addStream = addList.stream().map((x) -> x + "_outdated");
         System.out.println(Arrays.toString(addStream.toArray(String[]::new)));
+
+        //==============================================================================================================
 
         System.out.println("\nAdd _outdated to name");
         ArrayList<FileTest> addfilesList = new ArrayList<>(Arrays.asList(
@@ -62,16 +83,20 @@ public class StreamLabs2 {
                 new FileTest("1_outdated.txt", LocalDate.of(2001,11,3)),
                 new FileTest("1", LocalDate.of(2021,11,3))
         ));
-        Stream<FileTest> addfileStream = addfilesList.stream();
+        System.out.println(addfilesList);
+        Stream<FileTest> addfileStream = addfilesList.stream().peek( (x) -> {
+            if(!x.name.matches(".*_outdated(\\.[a-zA-Z0-9]+)?$")) {
+                Matcher matcher = Pattern.compile("\\.[a-zA-Z0-9]+$").matcher(x.name);
+                if(matcher.find()) {
+                    x.name = x.name.substring(0,matcher.start()) + "_outdated" +
+                            x.name.substring(matcher.start(), matcher.end());
+                }
+                else x.name = x.name + "_outdated";
+            }
+        });
         System.out.println(Arrays.toString(addfileStream.toArray(FileTest[]::new)));
 
-        String asd = "qwe asd.txt";
-        Pattern pattern = Pattern.compile(".{1}\\w+$", Pattern.CASE_INSENSITIVE);
-
-        Matcher matcher = pattern.matcher(asd);
-        while(matcher.find()) {
-            System.out.println(asd.substring(matcher.start(), matcher.end()));
-        }
+        //==============================================================================================================
 
     }
 
