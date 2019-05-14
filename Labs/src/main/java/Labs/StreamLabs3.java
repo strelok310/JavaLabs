559973d6a9c89696754e0806b401ab86f926e65a
Strelok310 <roman20330@mail.ru>
2019-05-14 15:12:14 +0300

    update

--- Labs/src/main/java/Labs/StreamLabs3.java
+++ Labs/src/main/java/Labs/StreamLabs3.java
@@ -9,9 +9,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamLabs3 {
+    private static final String LINE = "\n==============================================================================================================\n";
+
     public static void main(String args[]) throws Exception {
         task1();
         task2();
@@ -20,7 +24,8 @@ public class StreamLabs3 {
     }
 
     static void task1() {
-        System.out.println("\nReturn first element of array");
+        System.out.println(LINE);
+        System.out.println("Return first element of array");
         ArrayList<UserData> dataList = new ArrayList<>(Arrays.asList(
                 new UserData(15,"Image.jpg", LocalDate.of(2019,5,1)),
                 new UserData(9,"Star_Wars.mkv", LocalDate.of(2019,7,13)),
@@ -36,7 +41,7 @@ public class StreamLabs3 {
 
         UserData item = dataList.stream().findFirst().orElse(new UserData(3,"Picture.png", LocalDate.of(2018,6,15)));
 
-        System.out.println("Output:");
+        System.out.println("\nOutput:");
         System.out.println(item.toStringItem());
 
         //==============================================================================================================
@@ -44,12 +49,13 @@ public class StreamLabs3 {
         dataList.clear();
         item = dataList.stream().findFirst().orElse(new UserData(3,"Picture.png", LocalDate.of(2018,6,15)));
 
-        System.out.println("Output (not found):");
+        System.out.println("\nOutput (not found):");
         System.out.println(item.toStringItem());
     }
 
     static void task2() {
-        System.out.println("\nReturn first element of array");
+        System.out.println(LINE);
+        System.out.println("Return first element of array");
         ArrayList<UserData> list = new ArrayList<>(Arrays.asList(
                 new UserData(15,"Image.jpg", LocalDate.of(2019,5,1)),
                 new UserData(9,"Star_Wars.mkv", LocalDate.of(2019,7,13)),
@@ -71,7 +77,7 @@ public class StreamLabs3 {
             return false;
         }).findFirst().orElse(null);
 
-        System.out.println("Output:");
+        System.out.println("\nOutput:");
         System.out.println(item.toStringItem());
 
         //==============================================================================================================
@@ -82,7 +88,7 @@ public class StreamLabs3 {
             return false;
         }).findFirst().orElse(null);
 
-        System.out.println("Output (not found):");
+        System.out.println("\nOutput (not found):");
         if(item == null) System.out.println("Null");
         else System.out.println("not Null");
 
@@ -90,11 +96,14 @@ public class StreamLabs3 {
     }
 
     static void task3() throws Exception {
-        System.out.println("\nCount words and sort");
+        System.out.println(LINE);
+        System.out.println("Count words and sort");
         Stream<String> fileStream = Files.lines(Paths.get(ClassLoader.getSystemResource("wordCount.txt").toURI()));
 
         System.out.println("Intput:\nwordCount.txt");
 
+        //Variant 1
+        System.out.println("\nVariant 1:");
         HashMap<String, Integer> map = new HashMap<>();
         String[] words = fileStream
             .flatMap((x) -> Arrays.stream(x.split(" ")))
@@ -110,15 +119,45 @@ public class StreamLabs3 {
         System.out.println(Arrays.toString(words));
         System.out.println("Word frequency:");
         System.out.println(map);
+
+        //==============================================================================================================
+
+        //Variant 2
+        System.out.println("\nVariant 2:");
+        fileStream = Files.lines(Paths.get(ClassLoader.getSystemResource("wordCount.txt").toURI()));
+        Stream<String> res = fileStream.flatMap((x) -> Arrays.stream(x.split(" ")))
+                                            .collect(Collectors.groupingBy(String::toString, Collectors.counting()))
+                                            .entrySet()
+                                            .stream()
+                                            .sorted((x,y) -> Integer.parseInt(String.valueOf(y.getValue() - x.getValue())))
+                                            .map((x) -> x.getKey());
+        System.out.println(Arrays.toString(res.toArray(String[]::new)));
+
     }
 
     static void task4() throws Exception {
-        Stream<Integer> stream = Stream.of(1,2,3,4,5,6,7,8);
-        //ArrayList<Integer> list = stream.collect(Collectors.toList());
+        System.out.println(LINE);
+        System.out.println("Create ArrayList from stream");
+        Integer[] mas = {1,2,3,4,5,6,7,8};
 
+        System.out.println("\nVariant 1:");
+        ArrayList<Integer> list = new ArrayList<>(Arrays.stream(mas).collect(Collectors.toList()));
+        System.out.println(list);
+
+        System.out.println("\nVariant 2:");
+        list = Arrays.stream(mas).collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(list);
+
+        System.out.println("\nVariant 3:");
+        list = Arrays.stream(mas).collect(Collector.of( ArrayList::new,
+                                                        (x, item) -> x.add(item),
+                                                        (x, y) -> { x.addAll(y); return x; }));
+        System.out.println(list);
     }
 
+    static void task5() throws Exception {
 
+    }
 
 
 }
