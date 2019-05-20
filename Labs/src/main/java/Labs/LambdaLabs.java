499060d07c581066a6aa7bc67f1aa72c3dfb3b72
Strelok310 <roman20330@mail.ru>
2019-05-20 18:54:08 +0300

    update

--- Labs/src/main/java/Labs/LambdaLabs.java
+++ Labs/src/main/java/Labs/LambdaLabs.java
@@ -3,11 +3,14 @@ package Labs;
 import Utils.Lambdas.CallFunction;
 import Utils.Tmp;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LambdaLabs {
     private static final String LINE = "\n==============================================================================================================\n";
@@ -15,6 +18,12 @@ public class LambdaLabs {
     public static void main(String[] args) throws Exception {
         taskTest1();
         taskTest2();
+        taskTest3();
+        taskTest4();
+        taskTest5();
+        taskTest6();
+        taskTest7();
+        taskTest8();
     }
 
     /**
@@ -36,15 +45,178 @@ public class LambdaLabs {
      */
 
     static Consumer<String> task2() {
-        return x -> System.out.println("Hello " + x + " !");
+        return x -> System.out.println("Hello " + x + "!");
     }
 
     static void taskTest2() {
         System.out.println(LINE);
         System.out.println("Greet someone");
-        task2().accept("Roman");
+        task2().accept("Roma");
     }
 
+    /**
+     * Вернуть лямбду, которая принимает в качестве аргумента предыдущую лямбду, и добавляет вывод
+     * “Have a nice day!” после выполнения лямбды
+     */
+
+    static UnaryOperator<Consumer<String>> task3() {
+        return x -> x.andThen(y -> System.out.println("Have a nice day!"));
+    }
+
+    static void taskTest3() {
+        System.out.println(LINE);
+        System.out.println("Add \"Have a nice day!\"");
+        task3().apply(task2()).accept("Roma");
+    }
+
+    /**
+     * Вернуть лямбду, которая возвращает текущее время
+     */
+
+    static Supplier<LocalTime> task4() {
+        return () -> LocalTime.now();
+    }
+
+    static void taskTest4() {
+        System.out.println(LINE);
+        System.out.println("Return current time");
+        System.out.println("Current time: " + task4().get());
+    }
+
+    /**
+     * Вернуть лямбду, которая проверяет строку на наличие в ней email. Под email понимать любую строку,
+     * которая ввыглядит как <любые символы>@<любые символы>.<любые символы>
+     */
+
+    static Predicate<String> task5() {
+        //return x -> x.matches("[a-zA-Z0-9]+@(\\w+\\.[a-zA-Z]+)+");
+        return x -> x.matches("[\\w&&[^-_]](-?[\\w&&[^-]]+)*@(\\w+\\.[a-zA-Z]+)+");
+    }
+
+    static void taskTest5() {
+        System.out.println(LINE);
+        System.out.println("Check email");
+
+        String[] emails = {
+            "123@123.by",
+            "asdQWE@gmail.com",
+            "123_456@gmail.com",
+            "not@email@some.com",
+            "some|random\"characters.com",
+            "panda@zoo",
+            "snowfall@winter.123",
+            "new-againt@mail.ru",
+            "games@game.informer.com",
+            "new-@domain.org",
+            "-last@some.dt",
+            "a@b.com"
+        };
+
+        Predicate<String> matcher = task5();
+        for(String item : emails) {
+            System.out.println("Email: " + item + "\t\tCorrect: " + matcher.test(item));
+        }
+    }
+
+    /**
+     * К предыдущей лямбде добавить проверку на длину строки (> 10)
+     */
+
+    static UnaryOperator<Predicate<String>> task6() {
+        return x -> x.and(y -> y.length() > 10);
+    }
+
+    static void taskTest6() {
+        System.out.println(LINE);
+        System.out.println("Check email and length");
+
+        String[] emails = {
+                "123@123.by",
+                "asdQWE@gmail.com",
+                "123_456@gmail.com",
+                "not@email@some.com",
+                "some|random\"characters.com",
+                "panda@zoo",
+                "snowfall@winter.123",
+                "new-againt@mail.ru",
+                "games@game.informer.com",
+                "new-@domain.org",
+                "-last@some.dt",
+                "a@b.com"
+        };
+
+        Predicate<String> matcher = task6().apply(task5());
+        //Predicate<String> matcher = task5().and(task6());
+        for(String item : emails) {
+            System.out.println("Email: " + item + "\t\tCorrect: " + matcher.test(item));
+        }
+    }
+
+    /**
+     * К предыдущей лямбде добавить проверку на длину строки (> 10)
+     */
+
+    static UnaryOperator<Predicate<String>> task7() {
+        return x -> x.negate();
+    }
+
+    static void taskTest7() {
+        System.out.println(LINE);
+        System.out.println("Negate Check email and length");
+
+        String[] emails = {
+                "123@123.by",
+                "asdQWE@gmail.com",
+                "123_456@gmail.com",
+                "not@email@some.com",
+                "some|random\"characters.com",
+                "panda@zoo",
+                "snowfall@winter.123",
+                "new-againt@mail.ru",
+                "games@game.informer.com",
+                "new-@domain.org",
+                "-last@some.dt",
+                "a@b.com"
+        };
+
+        Predicate<String> matcher = task7().apply(task6().apply(task5()));
+        //Predicate<String> matcher = task5().and(task6()).negate(task7());
+        for(String item : emails) {
+            System.out.println("Email: " + item + "\t\tCorrect: " + matcher.test(item));
+        }
+    }
+
+    /**
+     * Вернуть лямбду, которая проверяет число на принадлежность к ряду Фибоначчи.
+     * Лямбда должна запоминать ранее вычисленные значения.
+     */
+
+    static Supplier<Predicate<Integer>> task8() {
+        return () -> {
+            ArrayList<Integer> list = new ArrayList<>(Arrays.asList(0,1));
+            return (x) -> {
+                while(list.get(list.size()-1) < x) {
+                    int id1 = list.size()-2;
+                    int id2 = list.size()-1;
+                    list.add(list.get(id1) + list.get(id2));
+                }
+                return list.contains(x);
+            };
+        };
+    }
+
+    static void taskTest8() {
+        System.out.println(LINE);
+        System.out.println("Check affiliation Fibonacci");
+
+        Predicate<Integer> fibonacci = task8().get();
+        System.out.println("13 (t): " + fibonacci.test(13));
+        System.out.println("5 (t): " + fibonacci.test(5));
+        System.out.println("15 (f): " + fibonacci.test(15));
+        System.out.println("55 (t): " + fibonacci.test(55));
+        System.out.println("34 (t): " + fibonacci.test(34));
+        System.out.println("26 (f): " + fibonacci.test(26));
+    }
 
 
     //==============================================================================
