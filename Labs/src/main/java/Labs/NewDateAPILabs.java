c22ba69afaa4f2df9d4c0f2ca22d61748274000e
Strelok310 <roman20330@mail.ru>
2019-05-24 13:07:09 +0300

    update

--- Labs/src/main/java/Labs/NewDateAPILabs.java
+++ Labs/src/main/java/Labs/NewDateAPILabs.java
@@ -3,6 +3,8 @@ package Labs;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 
 public class NewDateAPILabs {
     private static final String LINE = "\n==============================================================================================================\n";
@@ -15,6 +17,14 @@ public class NewDateAPILabs {
         task5();
         task6();
         task7();
+        task8();
+        task9();
+        task10();
+        task11();
+        task12();
+        task13();
+        task14();
+        task15();
     }
 
     /**
@@ -101,4 +111,131 @@ public class NewDateAPILabs {
         ZonedDateTime zonedDateTime = ZonedDateTime.parse("2007-12-03T10:15:30+03:00");
         System.out.println(zonedDateTime);
     }
+
+    /**
+     * Получить строку в формате ISO 8601 для текущей даты и времени для региона UTC
+     */
+
+    static void task8() {
+        System.out.println(LINE);
+        System.out.println("8) Get string in ISO 8601 with date and time for UTC\n");
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("UTC"));
+        System.out.println(zonedDateTime);
+        //System.out.println(zonedDateTime.format(DateTimeFormatter.ISO_DATE_TIME));
+    }
+
+    /**
+     * Получить строку в формате ISO 8601 для текущей даты и времени для региона Europe/Minsk
+     */
+
+    static void task9() {
+        System.out.println(LINE);
+        System.out.println("9) Get string in ISO 8601 with date and time for Europe/Minsk\n");
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("Europe/Minsk"));
+        System.out.println(zonedDateTime);
+        //System.out.println(zonedDateTime.format(DateTimeFormatter.ISO_DATE_TIME));
+    }
+
+    /**
+     * Добавить к текущей дате для региона UTC одну декаду
+     */
+
+    static void task10() {
+        System.out.println(LINE);
+        System.out.println("10) Add decade to current UTC date\n");
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("UTC")).plusDays(10);
+        System.out.println(zonedDateTime);
+    }
+
+    /**
+     * Получить Duration между текущей датой для регионов UTC и Europe/Minsk
+     */
+
+    static void task11() {
+        System.out.println(LINE);
+        System.out.println("11) Get Duration between UTC and Europe/Minsk\n");
+
+        ZonedDateTime dateUTC = ZonedDateTime.now(ZoneId.of("UTC"));
+        ZonedDateTime dateMinsk = ZonedDateTime.now(ZoneId.of("Europe/Minsk"));
+
+        System.out.println("UTC: " + dateUTC);
+        System.out.println("Europe/Minsk: " + dateMinsk);
+        System.out.println(Duration.between(dateUTC, dateMinsk));
+    }
+
+    /**
+     * Получить Duration между текущей датой для регионов UTC и GMT
+     */
+
+    static void task12() {
+        System.out.println(LINE);
+        System.out.println("12) Get Duration between UTC and GMT\n");
+
+        ZonedDateTime dateUTC = ZonedDateTime.now(ZoneId.of("UTC"));
+        ZonedDateTime dateGMT = ZonedDateTime.now(ZoneId.of("GMT"));
+
+        System.out.println("UTC: " + dateUTC);
+        System.out.println("GMT: " + dateGMT);
+        System.out.println(Duration.between(dateUTC, dateGMT));
+    }
+
+    /**
+     * Получить Period между текущей датой и вашим днем рождения (с годом)
+     */
+
+    static void task13() {
+        System.out.println(LINE);
+        System.out.println("13) Get Period between current date and birthday\n");
+
+        LocalDate birtday = LocalDate.of(1995,10,3);
+
+        System.out.println(birtday.until(LocalDate.now()));
+        System.out.println(Period.between(birtday, LocalDate.now()));
+    }
+
+    /**
+     * Получить дату следующего четверга
+     */
+
+    static void task14() {
+        System.out.println(LINE);
+        System.out.println("14) Get date of next thursday\n");
+
+        LocalDate date = LocalDate.of(2019,5,23)
+                //.now()
+                .with(TemporalAdjusters.next(DayOfWeek.THURSDAY));
+        System.out.println(date);
+    }
+
+    /**
+     * Получить дату второй субботы текущего месяца текущего года
+     */
+
+    static void task15() {
+        System.out.println(LINE);
+        System.out.println("15) Get date of second saturday of current month and year\n");
+
+        LocalDate date = LocalDate.now()
+                                  .with(TemporalAdjusters.firstInMonth(DayOfWeek.SATURDAY))
+                                  .plusWeeks(1);
+        System.out.println(date);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
